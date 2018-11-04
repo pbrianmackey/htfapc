cad1cc18c3415f71dc675e5f36d372ee56a5604c
Brian Mackey <mackey.brian@gmail.com>
2018-11-04 11:35:00 -0600

    Add Angular Material.  Use SCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { AboutComponent } from './about/about.component';
+import {MatCardModule, MatGridListModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,14 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatGridListModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.sass']
+  styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
 
