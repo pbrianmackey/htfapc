e8bbe28cd727673c7afd273c939eb63ec85a7cf9
Brian Mackey <mackey.brian@gmail.com>
2018-11-12 10:19:32 -0600

    Add theme.  Add sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { AboutComponent } from './about/about.component';
-import { MatCardModule, MatGridListModule, MatIconModule } from '@angular/material';
+import { MatCardModule, MatGridListModule, MatIconModule, MatCheckboxModule } from '@angular/material';
 import { MatMenuModule } from '@angular/material/menu';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SamplesComponent } from './samples/samples.component';
@@ -24,12 +24,14 @@ import { SamplesComponent } from './samples/samples.component';
     MatGridListModule,
     MatMenuModule,
     MatIconModule,
+    MatCheckboxModule,
     BrowserAnimationsModule
   ],
   exports: [
     MatCardModule,
     MatMenuModule,
     MatIconModule,
+    MatCheckboxModule,
     MatGridListModule
   ],
   providers: [],
