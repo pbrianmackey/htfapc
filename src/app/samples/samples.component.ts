6ac2deb4b730e173cf81562bdb6c0a44971460f2
Brian Mackey <mackey.brian@gmail.com>
2018-11-12 12:07:00 -0600

    Add selectors

--- src/app/samples/samples.component.ts
+++ src/app/samples/samples.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface Food {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-samples',
   templateUrl: './samples.component.html',
   styleUrls: ['./samples.component.scss']
 })
 export class SamplesComponent implements OnInit {
-
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' }
+  ];
   constructor() { }
 
   ngOnInit() {
